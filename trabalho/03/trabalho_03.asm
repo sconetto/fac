8f0be8b03d8ded8b714ef6f55082a9cb3d394b42
João Pedro Sconetto <sconetto.joao@gmail.com>
2017-10-11 10:42:27 -0300

    Adding some lines in the project

--- trabalho/03/trabalho_03.asm
+++ trabalho/03/trabalho_03.asm
@@ -13,7 +13,7 @@ pi: .float 3.1415926
 zero: .float 0.0
 positivo: .float 1.0
 negativo: .float -1.0
-precisao: .float 0.00000000001
+precisao: .float 0.00000000000000000001
 
 # Seção para a execução do processador
 .text
@@ -79,29 +79,43 @@ calc_arcsen:
   # t7 = auxiliar 3
   # f2 = reservado para calcular potencias
   add.s $f2, $f4, $f6        # Inicializando o registrador $f2 (a potência receberá uma série de multiplicações, logo inicializo com 1)
-  
+  add.s $f16, $f4, $f6       # Inicializando o registrador $f16 (o dividendo, logo inicializo com 1)
+  add.s $f18, $f4, $f6       # Inicializando o registrador $f18 (o divisor, logo inicializo com 1)
+  add.s $f20, $f4, $f6       # Inicializando o registrador $f20 (termo do divisor, logo inicializo com 1)
+  add.s $f22, $f4, $f4       # Inicializando o registrador $f22 (soma, logo inicializo com 0)
+  add.s $f28, $f4, $f6       # Inicializando o registrador $f27 (fatorial float, logo inicializo com 1)
+  add.s $f29, $f4, $f6       # Inicializando o registrador $f29 (fatorial float, logo inicializo com 1)
   add.s $f14, $f4, $f12      # Copiando o valor lido de $f12 para $f14
   
 loop_calc_arcsen:
   mul $t5, $t0, 2            # Calculo o valor de 2n 
-  jal fatorial               # Pula para calcular o fatorial de 2n
-  add $t1, $zero, $t3        # Adiciona a $t1 o resultado do cálculo de fatorial
-  addi $t3, $zero, 1         # Reinicializo a auxiliar $t3 de calcular fatoriais
+  jal fatorial_float         # Pula para calcular o fatorial de 2n
+  mul.s $f16, $f16, $f29     # Calcula o dividendo da minha operação
+#  add $t1, $zero, $t3        # Adiciona a $t1 o resultado do cálculo de fatorial
+#  addi $t3, $zero, 1         # Reinicializo a auxiliar $t3 de calcular fatoriais
   add $t5, $t0, $zero        # Calcula o valor de n
-  jal fatorial               # Pula para calcular o fatorial de n
-  mul $t2, $t2, $t3          # Multiplico no divisor o calculo de n!
-  mul $t2, $t2, $t2          # Como o meu divisor só tem n!, calculo o valor de (n!)^2
+  jal fatorial_float         # Pula para calcular o fatorial de n (float)
+  mul.s $f18, $f18, $f29     # Multiplico no divisor o calculo de n!
+  mul.s $f18, $f18, $f18      # Como o meu divisor só tem n!, calculo o valor de (n!)^2
+#  mul $t2, $t2, $t3          # Multiplico no divisor o calculo de n!
+#  mul $t2, $t2, $t2          # Como o meu divisor só tem n!, calculo o valor de (n!)^2
   mul $t5, $t0, 2            # Volto a calcular o valor de 2n
   addi $t5, $t5, 1           # Calculo o valor de 2n + 1
-  mul $t2, $t2, $t5          # Calculo no meu divisor a equação ((n!)^2)*(2n+1)
+  mtc1 $t5, $f29             # Movo o valor de 2n + 1 para o Coproc 1
+  cvt.s.w $f29, $f29         # Convertendo para float o valor de $f29
+  mul.s $f18, $f18, $f29     # Calculo no meu divisor a equação ((n!)^2)*(2n+1)
+#  mul $t2, $t2, $t5          # Calculo no meu divisor a equação ((n!)^2)*(2n+1)
   jal potencia_inteiro       # Para completar o meu divisor devo calcular 4^n, logo irei pular para a função potência e registrar em PC a chamada
-  mul $t2, $t2, $t4          # Calcula o resultado do divisor completo (((n!)^2)*(2n+1))*(4^n)
+  mtc1 $t4, $f29             # Pego o resultado de 4^n e coloco em $f29
+  cvt.s.w $f29, $f29         # Converto $f29 para float
+  mul.s $f18, $f18, $f29     # Calcula o resultado do divisor completo (((n!)^2)*(2n+1))*(4^n)
+#  mul $t2, $t2, $t4          # Calcula o resultado do divisor completo (((n!)^2)*(2n+1))*(4^n)
   mul $t5, $t0, 2            # Volto a calcular o valor de 2n
   addi $t5, $t5, 1           # Calculo o valor de 2n + 1
-  mtc1 $t1, $f16             # Movendo o dividendo de $t1 para $f16
-  cvt.s.w $f16, $f16         # Convertendo para float o valor de $f16
-  mtc1 $t2, $f18             # Movendo o divisor de $t2 para $f18
-  cvt.s.w $f18, $f18         # Convertendo para float o valor de $f18
+#  mtc1 $t1, $f16             # Movendo o dividendo de $t1 para $f16
+#  cvt.s.w $f16, $f16         # Convertendo para float o valor de $f16
+#  mtc1 $t2, $f18             # Movendo o divisor de $t2 para $f18
+#  cvt.s.w $f18, $f18         # Convertendo para float o valor de $f18
   jal potencia_x             # Para calcular o valor de x ^ (2n + 1)
   add.s $f20, $f4, $f2       # Copiando para o registrador $f20 o valor calculado na potencia_x
   # A partir de agora irei calcular o termo an da minha série e adicionar a soma final
@@ -111,11 +125,19 @@ loop_calc_arcsen:
   add.s $f24, $f24, $f22     # Adiciono ao meu registrador $f24 o valor dele com o do termo an.
   c.le.s $f22, $f26          # Verifica se o termo an é menor que a precisão desejada
   bc1t imprime_saida         # Caso o termo seja menor que a precisão, termina a execução e imprime a saída
+  bgtu $t0, 14, imprime_saida # Caso tenha iterado mais de 20 termos, termine a execução
   addi $t0, $t0, 1           # Adiciona um ao iterador
   addi $t1, $zero, 1         # Reinicializo a auxiliar $t1 de calcular o dividendo
   addi $t2, $zero, 1         # Reinicializo a auxiliar $t2 de calcular o divisor
   addi $t3, $zero, 1         # Reinicializo a auxiliar $t3 de calcular fatoriais
   addi $t4, $zero, 1         # Reinicializo a auxiliar $t4 de calcular potencias de 4 ^ n
+  add.s $f2, $f4, $f6        # Reinicializo o registrador $f2 (a potência receberá uma série de multiplicações, logo inicializo com 1)
+  add.s $f16, $f4, $f6
+  add.s $f18, $f4, $f6       # Reinicializo o registrador $f18 (o dividendo, logo inicializo com 1)
+  add.s $f20, $f4, $f6       # Reinicializo o registrador $f20 (termo do divisor, logo inicializo com 1)
+  add.s $f22, $f4, $f4       # Reinicializo o registrador $f22 (soma, logo inicializo com 0)
+  add.s $f28, $f4, $f6       # Reinicializo o registrador $218 (fatorial float, logo inicializo com 1)
+  add.s $f29, $f4, $f6       # Reinicializo o registrador $f29 (fatorial float, logo inicializo com 1)
   j loop_calc_arcsen         # Volto para o início do loop do cálculo do arcsens
 
 fatorial:
@@ -127,6 +149,20 @@ loop_fatorial:
   blez $t5, retorna_pc       # Caso $t5 seja zero ou menor volta pula para a função de retornar ao cálculo
   j loop_fatorial            # Itera até a condição de parada
   
+fatorial_float:
+  blez $t0, retorna_pc       # Verifico na chamada do fatorial se o meu termo é zero, caso sim eu retorno pois 0! = 1 e o registrador $t3 já terá 1 inicializado
+  mtc1 $t5, $f28             # Movendo o valor a se calcular o fatorial para $f28
+  cvt.s.w $f28, $f28         # Convertendo para float o valor de $f28
+  mtc1 $t3, $f29             # Movendo o 1 para o $f29 (auxiliar para cálculo do fatorial
+  cvt.s.w $f29, $f29         # Convertendo para float o valor de $f29
+  
+loop_fatorial_float:
+  mul.s $f29, $f29, $f28     # Multiplica o resultado pelo termo
+  add.s $f28, $f28, $f8      # Decrementa o termo para a próxima iteração
+  addi $t5, $t5, -1          # Decrementa o valor de n para verificação do final da iteração
+  blez $t5, retorna_pc       # Caso $t5 seja zero ou menor volta para a função de calcular o arcsen
+  j loop_fatorial_float      # Itera até a condição de parada
+
 retorna_pc:
   jr $ra                     # Retorna para onde o contador de programa (PC) estiver apontando 
 
